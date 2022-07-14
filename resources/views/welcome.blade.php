ee5242803cc5bd33e0e3c18944d42583f3dc0a2e
Nguyen Nhat Tay <taynn@vietis.com.vn>
2022-07-14 15:43:43 +0700

    build project

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ config('app.locale') }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Laravel</title>
+        <link rel="stylesheet" type="text/css" href="{{ mix('css/app.css') }}">
+    </head>
+    <body>
+        <div id="root">
+            Chính tại đây vue sẽ tìm đến và render page cho chúng ta !!!
+        </div>
+    </body>
+    <script type="text/javascript" src="{{ mix('js/app.js') }}"></script>
+</html>
